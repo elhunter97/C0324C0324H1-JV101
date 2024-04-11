01abd7d22c910035693396f3d600133f1145240c
elhunter97 <hanbaongoc14091271@gmail.com>
2024-04-11 09:57:40 +0700

    update

--- Module_1/exercise/7.1.js
+++ Module_1/exercise/7.1.js
@@ -1,5 +1,5 @@
 function convert(){
-    let cel = document.getElementById("c").value;
+    let cel = +document.getElementById("c").value;
     let f = (9*cel/5)+32;
     document.getElementById("result").innerHTML = "Result:"+f;
 }
